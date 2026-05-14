94cf1272c63e2fe3b745fe1a86240d8b6327c22d
Allan2468coder <oallan@byupathway.edu>
2026-05-14 21:31:31 +0300

    hamburger in homepage

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,11 @@
 // Dynamic footer
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
+
+// Mobile menu toggle
+const menuButton = document.getElementById("menuButton");
+const navMenu = document.getElementById("navMenu");
+
+menuButton.addEventListener("click", () => {
+  navMenu.style.display = navMenu.style.display === "flex" ? "none" : "flex";
+});
